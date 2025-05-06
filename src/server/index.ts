c405433275af7f35498d7106d648c499b70213fd
DeadMartyr <Angelo.S.Arriaga@pm.me>
2025-05-05 23:50:24 -0700

    Incorporated Handlebars Templating Engine NOTE: Handlebars-helpers introduced vulnerablities, we should cut it out if we can write our own

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,19 @@
+/**
+ * Project: NxN Battleship
+ * Authors: Angelo Arriaga
+ *          Rudy Liu
+ *
+ * Run site with `npm start`
+ */
+
+/* Dependency Imports */
 import express, { Request, Response, NextFunction } from "express"
 import http_errors from "http-errors"
+import path from "path"
+import * as exp_hbs from "express-handlebars"
+import Handlebars = require("handlebars");
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 /* Local Imports */
 import * as routes from "./routes"
@@ -8,11 +22,40 @@ import { mware_time } from "./middleware/time";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-/* Enable Middleware */
+
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+/* Allows express to parse/handle json
+ * by taking in application/json and returning the data in the body */
+app.use(express.json());
+/* Allows to take in url encoded data, like when a form is submitted */
+app.use(express.urlencoded({ extended: false }));
+
+/* Enable Middleware Functions */
 app.use(mware_time);
 
+/* Static Directory */
+app.use(express.static(path.join(process.cwd(), "public")));
+
+/* Routes */
 app.use("/", routes.root);
 app.use("testing", () => { });
+
+/* Handlebars rendering for pages */
+const hbs: exp_hbs.ExpressHandlebars = exp_hbs.create({
+  layoutsDir: path.join(__dirname, 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'views', 'partials'),
+  defaultLayout: 'layout_default',
+  extname: 'hbs',
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+/* Helpers for handlebars layouts */
+var helpers = require('handlebars-helpers')();
+Handlebars.registerHelper(helpers);
+
 
 app.use((_req: Request, _response: Response, next: NextFunction) => {
   next(http_errors(404));
--- src/server/routes/root.ts
+++ src/server/routes/root.ts
@@ -3,7 +3,19 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send(`Hello World from route in another file! (At ${res.locals.time_current})`);
+
+  //Do anything here relevant prior to rendering:
+  //i.e. figure out if dark mode is needed or not, pass in as "styles"
+
+  //RENDER
+  res.render("index", {
+    scripts: ['thing_A.js'],
+    styles: ['default.css'],
+
+    title: "Root Route Title!",
+    desc: `Hello World from route in another file! (At ${res.locals.time_current})`
+  });
+  ////////
 });
 
 export default router;
